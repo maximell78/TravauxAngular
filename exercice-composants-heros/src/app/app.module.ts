49a2d146e552013a728110e7ad700d5118162718
maximell78 <maximell78@users.noreply.github.com>
2021-10-08 16:17:42 -0400

    MaJ

--- exercice-composants-heros/src/app/app.module.ts
+++ exercice-composants-heros/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { EnteteComponent } from './entete/entete.component';
+import { RechercheComponent } from './recherche/recherche.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HeroComponent } from './hero/hero.component';
+import { PiedDePageComponent } from './pied-de-page/pied-de-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    EnteteComponent,
+    RechercheComponent,
+    CarouselComponent,
+    HeroComponent,
+    PiedDePageComponent
   ],
   imports: [
     BrowserModule,
