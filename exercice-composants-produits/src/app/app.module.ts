977e7ee5e19faa73800a6aba52e03e57c878f19b
maximell78 <maximell78@users.noreply.github.com>
2021-10-08 16:13:55 -0400

    final

--- exercice-composants-produits/src/app/app.module.ts
+++ exercice-composants-produits/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnteteComponent } from './entete/entete.component';
+import { ListeProduitsComponent } from './liste-produits/liste-produits.component';
+import { FicheProduitComponent } from './fiche-produit/fiche-produit.component';
+import { ImageComponent } from './image/image.component';
+import { DetailProduitComponent } from './detail-produit/detail-produit.component';
+import { PrixComponent } from './prix/prix.component';
+import { PiedDePageComponent } from './pied-de-page/pied-de-page.component';
+import { LienDetailsComponent } from './lien-details/lien-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnteteComponent,
+    ListeProduitsComponent,
+    FicheProduitComponent,
+    ImageComponent,
+    DetailProduitComponent,
+    PrixComponent,
+    PiedDePageComponent,
+    LienDetailsComponent
   ],
   imports: [
     BrowserModule,
