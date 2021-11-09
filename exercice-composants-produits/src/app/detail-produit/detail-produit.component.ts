392667b305c03b8f6eb71b2b1b9eb0eb8aa05a6a
maximell78 <maximell78@users.noreply.github.com>
2021-11-08 19:32:10 -0500

    MaJ

--- exercice-composants-produits/src/app/detail-produit/detail-produit.component.ts
+++ exercice-composants-produits/src/app/detail-produit/detail-produit.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./detail-produit.component.css']
 })
 export class DetailProduitComponent implements OnInit {
-  @Input() produit:String = '';
+  @Input() product:String = '';
 
   constructor() { }
 
